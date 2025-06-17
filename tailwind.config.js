e67f6e2a6abad79aba807b6edfd8f7e727952e8c
Luluparryboy2025 <luluparryboy@gmail.com>
2025-06-17 23:50:52 +1200

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,17 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#22d3ee",
+        background: "#1f2937",
+        panel: "#111827",
+      },
+    },
+  },
+  plugins: [],
+};
